ac31da0433a87bf66f6f262641f02ba67cf84c9b
Juan Infante Zumer <juan.zumer@thoughtworks.com>
2021-03-01 18:40:05 +0100

    Initial commit

---
+++ src/main/java/com/thoughtworks/reactiveatddworkshop/ReactiveAtddWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.reactiveatddworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAtddWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAtddWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/reactiveatddworkshop/ReactiveAtddWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.reactiveatddworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAtddWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
