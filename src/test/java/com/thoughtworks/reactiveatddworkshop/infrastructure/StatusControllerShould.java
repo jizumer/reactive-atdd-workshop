7c290ff86b2f9aca614a7d18f679d6b16e7eb165
Juan Infante Zumer <juan.zumer@thoughtworks.com>
2021-03-03 18:10:43 +0100

    First approach of integration tests using WebTestClient

---
+++ src/test/java/com/thoughtworks/reactiveatddworkshop/infrastructure/StatusControllerShould.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.reactiveatddworkshop.infrastructure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class StatusControllerShould {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    @DisplayName("Status result status should be 200 (OK)")
+    void returnStatusOk() {
+        webClient.get()
+                .uri("/status")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .expectBody()
+                .jsonPath("$.numberOfStatusRequests").isNotEmpty()
+                .jsonPath("$.startTimestamp").isNotEmpty();
+    }
+
+}
