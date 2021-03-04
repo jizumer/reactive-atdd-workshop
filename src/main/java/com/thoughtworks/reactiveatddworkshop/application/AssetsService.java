5e0557f31cead14b25c0fec6e84ab4dc3f5448f1
Juan Infante Zumer <juan.zumer@thoughtworks.com>
2021-03-04 17:13:31 +0100

    Assets endpoint

--- src/main/java/com/thoughtworks/reactiveatddworkshop/application/AssetsService.java
+++ src/main/java/com/thoughtworks/reactiveatddworkshop/application/AssetsService.java
@@ -5,6 +5,7 @@ import com.thoughtworks.reactiveatddworkshop.infrastructure.AssetsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class AssetsService {
@@ -13,6 +14,33 @@ public class AssetsService {
     @Autowired
     public AssetsService(AssetsRepository assetsRepository) {
         this.assetsRepository = assetsRepository;
+    }
+
+    public Mono<Asset> createAsset(Asset asset) {
+        return assetsRepository.save(asset);
+    }
+
+    public Flux<Asset> getAllAssets() {
+        return assetsRepository.findAll();
+    }
+
+    public Mono<Asset> findById(String assetId) {
+        return assetsRepository.findById(assetId);
+    }
+
+    public Mono<Asset> updateAsset(String assetId, Asset asset) {
+        return assetsRepository.findById(assetId)
+                .flatMap(dbAsset -> {
+                    dbAsset.setAmount(asset.getAmount());
+                    dbAsset.setName(asset.getName());
+                    return assetsRepository.save(dbAsset);
+                });
+    }
+
+    public Mono<Asset> deleteAsset(String assetId) {
+        return assetsRepository.findById(assetId)
+                .flatMap(existingAsset -> assetsRepository.delete(existingAsset)
+                        .then(Mono.just(existingAsset)));
     }
 
     public Flux<Asset> findByAssetName(String assetName) {
--- src/main/java/com/thoughtworks/reactiveatddworkshop/infrastructure/AssetsController.java
+++ src/main/java/com/thoughtworks/reactiveatddworkshop/infrastructure/AssetsController.java
@@ -3,13 +3,15 @@ package com.thoughtworks.reactiveatddworkshop.infrastructure;
 import com.thoughtworks.reactiveatddworkshop.application.AssetsService;
 import com.thoughtworks.reactiveatddworkshop.domain.Asset;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@RestController(value = "/assets")
+@RestController
+@RequestMapping("/assets")
 public class AssetsController {
     private final AssetsService assetsService;
 
@@ -18,8 +20,34 @@ public class AssetsController {
         this.assetsService = assetsService;
     }
 
-    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Asset> create(@RequestBody Asset asset) {
+        return assetsService.createAsset(asset);
+    }
+
+    @GetMapping
+    public Flux<Asset> getAllAssets() {
+        return assetsService.getAllAssets();
+    }
+
+    @GetMapping("/{assetId}")
+    public Mono<ResponseEntity<Asset>> getAssetById(@PathVariable String assetId) {
+        Mono<Asset> user = assetsService.findById(assetId);
+        return user.map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "/name/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Asset> getAssetsByName(@PathVariable String name) {
         return assetsService.findByAssetName(name);
     }
+
+    @DeleteMapping("/{assetId}")
+    public Mono<ResponseEntity<Void>> deleteUserById(@PathVariable String assetId) {
+        return assetsService.deleteAsset(assetId)
+                .map(r -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
 }
