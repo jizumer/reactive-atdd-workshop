4fb737e957a5c62c9d6d39550878939ac37f1e08
Carlos Cavero <cavero.carlos@gmail.com>
2021-03-09 11:59:58 +0100

    Configure properly the output of the report and remove unneeded comments

--- src/test/java/com/thoughtworks/reactiveatddworkshop/acceptance/CucumberTest.java
+++ src/test/java/com/thoughtworks/reactiveatddworkshop/acceptance/CucumberTest.java
@@ -1,16 +1,3 @@
-/*
-  Copyright 2019 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
 package com.thoughtworks.reactiveatddworkshop.acceptance;
 
 import com.vimalselvam.cucumber.listener.Reporter;
@@ -24,7 +11,7 @@ import java.io.File;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
     format = "pretty",
-    plugin = "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/reports/report.html")
+    plugin = "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:build/reports/cucumber-report.html")
 public class CucumberTest {
     @AfterClass
     public static void teardown() {
