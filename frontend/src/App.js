33a76fcbefa832d694b25be357a9bca325da9f5a
Juan Infante Zumer <juan.zumer@thoughtworks.com>
2021-09-09 08:46:52 +0200

    Added a react-query command integrated with react useState Hook.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
-import react, { Component } from "react";
+import react from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [status, setStatus] = react.useState(0);
+const queryClient = new QueryClient();
 
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Application status: <code>{status}</code>
-        </p>
-        <button onClick={() => setStatus(status + 1)}>Update status</button>
+
+        <QueryClientProvider client={queryClient}>
+          <Status />
+        </QueryClientProvider>
       </header>
+    </div>
+  );
+}
+
+function Status() {
+  const [numberOfStatusRequests, setNumberOfStatusRequests] = react.useState(0);
+  const [startTimestamp, setStartTimestamp] = react.useState("Init");
+
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("http://localhost:8081/status").then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>
+        Application status requests: <code>{numberOfStatusRequests}</code>
+      </h1>
+      <h2>
+        Last time updated: <time>{startTimestamp}</time>
+      </h2>
+
+      <button
+        onClick={() => {
+          setNumberOfStatusRequests(data.numberOfStatusRequests);
+          setStartTimestamp(data.startTimestamp);
+        }}>
+        Update status
+      </button>
     </div>
   );
 }
--- frontend/src/api/index.js
+++
@@ -1,8 +0,0 @@
-export const getStatus = async () => {
-	try{
-        const resp = await fetch('http://localhost:8080/status');
-        return resp;
-	}catch(error){
-		throw error;
-	}
-}
